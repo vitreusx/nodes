13f05afdcb06239169ea8dcceb8f610045b4adcc
cvybhu <>
2020-03-17 17:09:48 +0100

    Basic nodectl -> noded command gets send

--- nodectl.py
+++ nodectl.py
@@ -1,9 +1,44 @@
 """
 nodectl is a command line interface for nodedaemon
 """
+import requests 
+
+nodedAddress = "http://localhost:5000"
+
+def command_help():
+    print("do <command> - do command on connected noded")
+    print("send <command> <name> - send command to other node with given name")
+
+def command_do(command):
+    doRequest = requests.post(f"{nodedAddress}/api/docommand", data={'cmd': command})
+
+def command_send(command, name):
+    sendRequest = requests.post(f"{nodedAddress}/api/sendcommand", data={'cmd': command, 'target': name})
+    return
 
 if __name__ == '__main__':
+    print("Connecting to noded...");
+    nameRequest = requests.get(f"{nodedAddress}/api/name")
+    
+    if nameRequest.status_code != 200:
+        print(f"ERROR: response status: {nameRequest.status_code}, text: {nameRequest.text}")
+
+    nodeName = nameRequest.text
+    print(f"Succesfully connected to noded with name '{nodeName}'")
+    print("Starting command line - type 'help' for help")
+
     while True:
         print("> ", end="")
         command = input()
         print("Doing the command: " + command)
+
+        if(command == "help"):
+            command_help()
+        
+        cmd = command.split(' ')
+
+        if(cmd[0] == "do"):
+            command_do(cmd[1])
+
+        if(cmd[0] == "send"):
+            command_send(cmd[1], cmd[2])
--- noded.py
+++ noded.py
@@ -6,24 +6,51 @@ Daemon handles node stuff and serves web interface
 
 from flask import Flask, request;
 import sqlite3 as sql;
+import click
+from flask.cli import AppGroup
+import requests
 
 app = Flask(__name__, static_folder='data/html')
 
 db_path = "data/noded_db.db"
 
+known_nodes = [
+    {'name': 'nodeA', 'ip': "127.0.0.1", 'port': 5000},
+    {'name': 'nodeB', 'ip': "127.0.0.1", 'port': 5001}
+]
+
+nodeName = "nodeA"
+
+# API Requests
+
+@app.route('/api/name', methods = ['GET'])
+def on_api_name():
+    return nodeName;
+
+@app.route('/api/docommand', methods = ['POST'])
+def on_api_docommand():
+    command = request.values.get('cmd')
+    print("Doing command " + str(command))
+    # Do the command
+    return "OK"
+
+@app.route('/api/sendcommand', methods = ['POST'])
+def on_api_sendcommand():
+    command = request.values.get('cmd')
+    target = request.values.get('target') 
+    targetNode = [n for n in known_nodes if n['name'] == target][0]
+    doRequest = requests.post(f"{targetNode['ip']}:{targetNode['port']}/api/docommand", data={'cmd': command})
+    return "I dont know how to do that"
+
+@app.route('/api/nodes', methods = ['GET'])
+def on_api_nodes():
+    # Send known nodes somehow, Json? Split by some character?
+    return "YES"
+
 @app.route('/') # Serves web interface
 def home():
-   return app.send_static_file('index.html')
-
-@app.route('/command') # Receives command to execute
-def on_command():
-   command = request.args.get('cmd')
-   return "Ok doing " + str(command) + "\n"
-
-@app.route('/conn_request') # Request for connection
-def on_connect_request():
-   return "NO"
+    return app.send_static_file('index.html')
 
 if __name__ == '__main__':
-    con = sql.connect(db_path)
-    app.run()
+     con = sql.connect(db_path)
+     app.run()
