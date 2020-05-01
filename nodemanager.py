4d8e4371d82b56f7d344d5777e98b45c06973138
cvybhu <>
2020-05-01 19:33:40 +0200

    Added tests for nodemanager

--- nodemanager.py
+++ nodemanager.py
@@ -1,7 +1,7 @@
 import yaml
 
 class NodeManager:
-    config_file = "config.yaml"
+    config_path = "config.yaml"
     db_file = "data/data.db"
 
     # Node data
@@ -13,20 +13,23 @@ class NodeManager:
 
     known_nodes = []
 
-    def __init__(self):
+    def __init__(self, config_file_path = 'config.yaml'):
+        self.config_path = config_file_path
         self.read_config()
 
     def read_config(self):
-        the_file = open(self.config_file, 'r')
+        the_file = open(self.config_path, 'r')
         config_yaml = yaml.safe_load(the_file)
 
         self.nodeName = config_yaml['settings']['name']
         self.node_port = config_yaml['settings']['port']
         self.recognize_voice = config_yaml['settings']['recognize_voice']
 
+        self.commands = []
         for command in config_yaml['commands']:
             self.commands.append((command['command'], command['script']))
 
+        self.known_nodes = []
         for known_node in config_yaml['known_nodes']:
             self.known_nodes.append(known_node)
 
@@ -48,7 +51,7 @@ class NodeManager:
                                     'script': script
                                     })
 
-        the_file = open(self.config_file, 'w')
+        the_file = open(self.config_path, 'w')
         yaml.dump(config, the_file, 
                   default_flow_style=False, sort_keys=False)
 
@@ -68,20 +71,12 @@ class NodeManager:
         self.node_port = new_port
         self.write_config()
 
-    # Known nodes
-    def get_known_nodes(self):
-        return self.known_nodes
+    # Voice
+    def should_recognize_voice(self):
+        return self.recognize_voice
 
-    def add_known_node(self, node_name, node_ip, node_port):
-        new_node = {'name': node_name, 'ip': node_ip, 'port': node_port}
-
-        if not new_node in self.known_nodes:
-            self.known_nodes.append(new_node)
-        
-        self.write_config()
-
-    def remove_known_node(self, node_name):
-        known_nodes = [node for node in known_nodes if node['name'] != node_name]
+    def set_recognize_voice(self, should_recoginze):
+        self.recognize_voice = should_recoginze
         self.write_config()
 
     # Commands
@@ -97,10 +92,26 @@ class NodeManager:
     def get_commands(self):
             return self.commands
 
-    # Voice
-    def should_recoginze_voice(self):
-        return self.recognize_voice
+    def add_command(self, command_name, command_script):
+        self.commands.append((command_name, command_script))
+        self.write_config()
 
-    def set_recognize_voice(self, should_recoginze):
-        self.recognize_voice = should_recoginze
+    def remove_command(self, command_name):
+        self.commands = [(name, script) for (name, script) in self.commands if name != command_name]
+        self.write_config()
+
+    # Known nodes
+    def get_known_nodes(self):
+        return self.known_nodes
+
+    def add_known_node(self, node_name, node_ip, node_port):
+        new_node = {'name': node_name, 'ip': node_ip, 'port': node_port}
+
+        if not new_node in self.known_nodes:
+            self.known_nodes.append(new_node)
+        
+        self.write_config()
+
+    def remove_known_node(self, node_name):
+        self.known_nodes = [node for node in self.known_nodes if node['name'] != node_name]
         self.write_config()
