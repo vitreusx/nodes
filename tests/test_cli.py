874f9fdba2e2a40941eacdd8f3f83bef0e6d381f
artur234 <jaskier234@gmail.com>
2020-05-21 11:36:03 +0200

    Voice command tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -25,3 +25,15 @@ class TestCli(unittest.TestCase):
         self.cli_call(self.cli.do_group, ['-d', 'grupa2'], 'Success')
         self.cli_call(self.cli.do_group, ['-d', 'grupa3'], 'Success')
         self.cli_call(self.cli.do_list, [], '')
+
+    def test_voice_add(self):
+        self.cli_call(self.cli.do_voice, ['-a', 'kom1', 'edp1', 'p'], 'Success')
+        self.cli_call(self.cli.do_voice, ['-a', 'kom2', 'edp1', 'p'], 'Success')
+        self.cli_call(self.cli.do_voice, ['-a', 'kom3', 'edp1', 'p'], 'Success')
+        
+
+    def test_voice_remove(self):
+        self.cli_call(self.cli.do_voice, ['-r', 'kom1'], 'Success')
+        self.cli_call(self.cli.do_voice, ['-r', 'kom2'], 'Success')
+        self.cli_call(self.cli.do_voice, ['-r', 'kom3'], 'Success')
+        
