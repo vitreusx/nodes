735369593a806132a48a6f3e83f942ed72c8786a
talos <darksoldierpl@outlook.com>
2020-05-22 10:44:42 +0200

    hotfix

---
+++ sbox/Exec/tasks/__init__.py
@@ -0,0 +1,10 @@
+from node.nexus import Nexus
+from flask import request as req
+import webbrowser as wb
+
+class Local:
+    def __init__(self, nx: Nexus):
+        @nx.app.route('/tasks/lights', methods=['POST'])
+        def lights():
+            print(f'New lights\' state: {req.json.get("state") or "N/A"}')
+            return ''
