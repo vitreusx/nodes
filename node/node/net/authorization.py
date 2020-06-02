639cb3d117abbc588e605545e8b4fd73dc0a9659
cvybhu <>
2020-06-02 11:40:20 +0200

    Prototype of authorization class ready

--- node/node/net/authorization.py
+++ node/node/net/authorization.py
@@ -1,31 +1,52 @@
 import shelve
 
+"""
+    Authorization works using users
+    There is a set of authorized users that are allowed to use this node
+    Other nodes present their name and accessToken as username and password
+    There is an user named 'local' with random password that local interfaces use to authorize
+
+"""
+
 class Authorization:
     def __init__(self):
+        # db['users'] is a dict username -> password of trusted users
+        # db['tokens'] is a dict nodeName -> accessToken keeping this node's access tokens to other nodes
         self.db = shelve.open('db/auth.db', writeback=True)
 
+        if(not self.db.has_key('users')):
+            self.db = {'users': {}, 'tokens': {}}
+
+        if(not self.db['users'].has_key('local')):
+            self.db['users']['local'] = "12345" # TODO - change to random
+
     def validate_user(self, username, password):
-        if not (username and password):
+        if(not self.db['users'].has_key(username)):
             return False
 
-        if(not self.db.has_key('master')):  # TODO - remove this
-            self.db['master'] = 'masterpass'
-
-        if(not self.db.has_key(username)):
-            return False
-
-        return self.db[username] == password
+        return (self.db[username] == password)
 
     def add_user(self, username, password):
-        self.db[username] = password
+        self.db['users'][username] = password
     
     def remove_user(self, username):
-        if(self.db.has(username)):
+        if(self.db['users'].has(username)):
             del self.db[username]
 
+    def get_local_user_pass(self):
+        return self.db['users']['local']
+
     def get_user_list(self):
-        return db.keys()
+        return self.db['users'].keys()
 
     def clear_users(self):
-        self.db.clear()
+        self.db['users'].clear()
 
+    def add_access_token(self, nodeName, accessToken):
+        self.db['tokens'][nodeName] = accessToken
+
+    def get_access_token_for(self, nodeName):
+        return self.db['tokens'].get(nodeName)
+
+    def clear_access_tokens(self):
+        self.db['tokens'].clear()
