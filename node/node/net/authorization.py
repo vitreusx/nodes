a20a902baecca84df9f180e6c53bdd60e2fe6829
cvybhu <>
2020-06-03 18:48:41 +0200

    Update tutorial

--- node/node/net/authorization.py
+++ node/node/net/authorization.py
@@ -22,6 +22,8 @@ class Authorization:
         makedirs(dirname('db/auth.db'), exist_ok=True)
         self.db = shelve.open('db/auth.db', writeback=True)
 
+        self.my_name = node_name
+
         if(self.db.get('users') is None):
             self.db['users'] = {}
             self.db['tokens'] = {}
@@ -43,12 +45,15 @@ class Authorization:
 
         return (self.db['users'][username] == password)
 
+
     def add_user(self, username, password):
         self.db['users'][username] = password
+        self.db.sync()
     
     def remove_user(self, username):
         if(self.db['users'].has(username)):
             del self.db[username]
+        self.db.sync()
 
     def get_local_user_pass(self):
         return self.db['users']['local']
@@ -58,15 +63,18 @@ class Authorization:
 
     def clear_users(self):
         self.db['users'].clear()
+        self.db.sync()
 
     def add_access_token(self, nodeName, accessToken):
         self.db['tokens'][nodeName] = accessToken
+        self.db.sync()
 
     def get_access_token_for(self, nodeName):
         return self.db['tokens'].get(nodeName)
 
     def clear_access_tokens(self):
         self.db['tokens'].clear()
+        self.db.sync()
 
     # Checks if there is authorization data for a given node
     def has_requests_auth(self, node_name):
@@ -89,5 +97,5 @@ class Authorization:
 
         cert_path = os.path.join('certs', f"{node_name}.pem")
 
-        return {'auth': (node_name, self.db['tokens'].get(node_name)),
+        return {'auth': (self.my_name, self.db['tokens'].get(node_name)),
                 'verify': str(cert_path)}
